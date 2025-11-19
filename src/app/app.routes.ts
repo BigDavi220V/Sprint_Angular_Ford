0185dca12e785a9e8830d3cf2493188877d1bf13
BigDavi220V <davi_santiagoo@hotmail.com>
2025-11-19 10:47:06 -0300

    Fiz a protecao de navegacao das rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,30 @@
 import { Routes } from '@angular/router';
+// 💡 Caminho corrigido para sua pasta 'auth'
+import { authGuard, unauthGuard } from './auth/auth.guard'; 
 
 export const routes: Routes = [
   {
     path: '',
     loadComponent:()=>
-      import('./pages/login/login.component').then(m=> m.LoginComponent)
+      import('./pages/login/login.component').then(m=> m.LoginComponent),
+    // 💡 Aplica o unauthGuard: Se já estiver logado, não pode voltar para o login.
+    canActivate: [unauthGuard] 
   },
 
-    {
+  {
     path: 'home',
     loadComponent:()=>
-      import('./pages/home/home.component').then(m=> m.HomeComponent)
+      import('./pages/home/home.component').then(m=> m.HomeComponent),
+    // 💡 Aplica o authGuard: Precisa estar logado para acessar a Home.
+    canActivate: [authGuard] 
   },
 
   {
     path: 'dashboard',
     loadComponent:()=>
-      import('./pages/dashboard/dashboard.component').then(m=> m.DashboardComponent)
+      import('./pages/dashboard/dashboard.component').then(m=> m.DashboardComponent),
+    // 💡 Aplica o authGuard: Precisa estar logado para acessar o Dashboard.
+    canActivate: [authGuard] 
   },
 
  
